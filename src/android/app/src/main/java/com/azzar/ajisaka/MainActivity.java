3a42f3fb89858764755b15e1aec76a57bd1e7aee
Azzar <azzar@github.local>
2026-08-16 16:37:01 +0700

    ci: auto-generate android apk via capacitor in github release

---
+++ src/android/app/src/main/java/com/azzar/ajisaka/MainActivity.java
@@ -0,0 +1,5 @@
+package com.azzar.ajisaka;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ src/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.azzar.ajisaka',
+  appName: 'Ajisaka',
+  webDir: 'dist'
+};
+
+export default config;
